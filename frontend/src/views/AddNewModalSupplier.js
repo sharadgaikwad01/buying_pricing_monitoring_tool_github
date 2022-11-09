14fb6169b53f5c01cb15c8a1261014d831e18b57
Tathod <rahul.tathod@metro-gsc.in>
2022-11-09 16:03:20 +0530

    changes

---
+++ frontend/src/views/AddNewModalSupplier.js
@@ -0,0 +1,148 @@
+import { User, Briefcase, Mail, Calendar, DollarSign, X, ChevronDown } from 'react-feather'
+
+import { Modal, Badge, Input, Label, Button, ModalHeader, ModalBody, InputGroup, InputGroupText, FormFeedback, Form } from 'reactstrap'
+
+// ** Styles
+
+import DataTable from 'react-data-table-component'
+import '@styles/react/libs/tables/react-dataTable-component.scss'
+import '@styles/react/libs/flatpickr/flatpickr.scss'
+
+import Swal from 'sweetalert2'
+import ReactPaginate from 'react-paginate'
+import withReactContent from 'sweetalert2-react-content'
+import { useState } from 'react'
+const MySwal = withReactContent(Swal)
+
+const AddNewModalSupplier = ({ open, handleModal, rowData }) => {
+  const [currentPage, setCurrentPage] = useState(0)
+  // const [supplierInputsData, setsupplierInputsData] = useState([])
+  const dataToRender = () => {
+    return rowData
+  }
+  // ** Function to handle Pagination
+  const handlePagination = page => {
+    setCurrentPage(page.selected)
+  }
+  const CustomPagination = () => (
+    <ReactPaginate
+      previousLabel=''
+      nextLabel=''
+      forcePage={currentPage}
+      onPageChange={page => handlePagination(page)}
+      pageCount={Math.ceil(dataToRender().length / 7) || 1}
+      breakLabel='...'
+      pageRangeDisplayed={2}
+      marginPagesDisplayed={2}
+      activeClassName='active'
+      pageClassName='page-item'
+      breakClassName='page-item'
+      nextLinkClassName='page-link'
+      pageLinkClassName='page-link'
+      breakLinkClassName='page-link'
+      previousLinkClassName='page-link'
+      nextClassName='page-item next-item'
+      previousClassName='page-item prev-item'
+      containerClassName='pagination react-paginate separated-pagination pagination-sm justify-content-end pe-1 mt-1'
+    />
+  )
+  
+  const columns = [
+    {
+      name: 'Row Id',
+      omit:true,
+      selector: row => row.row_id
+    },
+    {
+      name: 'Supplier No.',
+      sortable: true,
+      width: 'auto',
+      selector: row => row.suppl_no,
+      cell: row => {
+        return (
+          row.suppl_no
+        )
+      }
+    },
+    {
+      name: 'Supplier Name',
+      sortable: true,
+      minWidth: 'auto',
+      selector: row => row.suppl_name,
+      cell: row => {
+        return (
+          row.suppl_name ? row.suppl_name : "-"
+        )
+      }
+    },
+    {
+      name: 'Price Increase By Seller',
+      sortable: true,
+      minWidth: 'auto',
+      selector: row => row.price_difference_perc,
+      cell: row => {
+        return (
+          row.price_difference_perc ? `${row.price_difference_perc}%` : "-"
+        )
+      }
+    },
+    {
+      name: 'Aggreed Price',
+      sortable: true,
+      width: 'auto',
+      selector: row => row.agreed_price_increase_perc,
+      cell: row => {
+        return (
+          row.agreed_price_increase_perc ? `${row.agreed_price_increase_perc}%` : "-"
+        )
+      }
+    },    
+    {
+      name: 'Status',
+      width: 'auto',
+      sortable: row => row.action_status,
+      cell: row => {
+        return (
+          row.action_status === 'open' ? <Badge color='primary' pill>{row.action_status}</Badge> : <Badge color='success' pill>{row.action_status}</Badge>
+        )
+      }
+    }
+  ]
+
+  const CloseBtn = <X className='cursor-pointer' size={15} onClick={handleModal} />
+  return (
+    <Modal
+      isOpen={open}
+      toggle={handleModal}
+      className='sidebar-lg modal-lg'
+      modalClassName='modal-in'
+      contentClassName='pt-0 modal-lg'
+    >
+      <ModalHeader className='mb-1' toggle={handleModal} close={CloseBtn} tag='div'>
+        <h5 className='modal-title'>All Record</h5>
+      </ModalHeader>
+      <ModalBody className='flex-grow-1'>
+          <div className='react-dataTable my-1'>
+            <DataTable
+              noHeader
+              pagination
+              selectableRowsNoSelectAll
+              columns={columns}
+              paginationPerPage={50}
+              className='react-dataTable'
+              sortIcon={<ChevronDown size={10} />}
+              paginationDefaultPage={currentPage + 1}
+              paginationComponent={CustomPagination}
+              // data={searchValue.length ? filteredData : data}
+              data={dataToRender()}
+            />
+          </div>
+          <Button color='danger' onClick={handleModal} outline>
+            Close
+          </Button>
+      </ModalBody>
+    </Modal>
+  )
+}
+
+export default AddNewModalSupplier
--- frontend/src/views/View.js
+++ frontend/src/views/View.js
@@ -3,6 +3,7 @@ import { nodeBackend } from '@utils'
 // ** Add New Modal Component
 import { Download, Search, ChevronDown, Share, Printer, FileText, File, Grid, Copy, Plus, Upload, Edit, Trash, Check } from 'react-feather'
 import axios from 'axios'
+import AddNewModalSupplier from './AddNewModalSupplier'
 
 // ** Reactstrap Imports
 import {
@@ -22,45 +23,48 @@ import {
     Badge
 } from 'reactstrap'
 
-const countries = [
-    "RO", 
-    "SK", 
-    "DE", 
-    "CZ", 
-    "IT"
-]
-
-const queryParams = new URLSearchParams(window.location.search)
-const suppl_no = queryParams.get('sup')
-
-
 const View = () => {
+    const queryParams = new URLSearchParams(window.location.search)
+    const suppl_no = queryParams.get('sup')
+    const country = localStorage.getItem('country')
     const [Suppliername, setSuppliername] = useState('')
+    const [modal, setModal] = useState(false)
+    const handleModal = () => setModal(!modal)
     // const [Artical_no, setArtical_no] = useState('')
     // const [Art_description, setArt_description] = useState('')
     // const [Art_category, setArt_category] = useState('')
-    const [Current_price, setCurrent_price] = useState('')
-    const [New_price, setNew_price] = useState('')
-    const [Request_date, setRequest_date] = useState('')
-    const [Reason_Price_change, setReason_Price_change] = useState('')
-    
+    // const [Current_price, setCurrent_price] = useState('')
+    // const [New_price, setNew_price] = useState('')
+    const [Supplierno, setSupplierno] = useState('')
+    // const [Reason_Price_change, setReason_Price_change] = useState('')
+    const [rowData, setRowData] = useState([])
+    const [countries, setcountries] = useState([])
+
+
+    const handleEdit = async (e, countryname) => {
+        e.preventDefault()
+        handleModal()
+        console.log(countryname)
+        await axios.get(`${nodeBackend}/buyer_supplier_details_list`, { params: { countryname } }).then((res) => {
+            console.log(res.data.data)
+            setRowData(res.data.data)
+          })
+        //setRowData(id)
+    }
     useEffect(async () => {
         // if (!suppl_no) {
         //   props.history.push('/buyer_input')
         // }
     
-        await axios.get(`${nodeBackend}/buyer_supplier_details`, { params: { suppl_no } }).then((res) => {
+        await axios.get(`${nodeBackend}/buyer_supplier_details`, { params: { suppl_no, country } }).then((res) => {
           if (res.data.data) {
-            console.log(res.data.data.rows[0])
-            if (res.data.data.rows[0]) {
-                setSuppliername(res.data.data.rows[0].suppl_name)
-                // setArtical_no(res.data.data.rows[0].art_no)
-                // setArt_description(res.data.data.rows[0].art_name_tl)
-                // setArt_category(res.data.data.rows[0].suppl_name)
-                setCurrent_price(res.data.data.rows[0].current_price ? res.data.data.rows[0].current_price : "NA")
-                setNew_price(res.data.data.rows[0].new_price)
-                setRequest_date(res.data.data.rows[0].request_date)
-                setReason_Price_change(res.data.data.rows[0].price_change_reason)
+            console.log(res.data.data[0])
+            if (res.data.data[0]) {
+                setSuppliername(res.data.data[0].bdm_global_umbrella_name)
+                setSupplierno(res.data.data[0].bdm_global_umbrella_no)
+            }
+            if (res.data.response) {
+                setcountries(res.data.response)
             }
           }      
         })
@@ -70,7 +74,7 @@ const View = () => {
         <Fragment>
             <Card className='pageBox buyer-screen'>
                 <CardHeader className='flex-md-row flex-column align-md-items-center align-items-start border-bottom'>
-                    <CardTitle tag='h2'>Supplier Number - {suppl_no}</CardTitle>
+                    {/* <CardTitle tag='h2'>Supplier Number - {suppl_no}</CardTitle> */}
                 </CardHeader>
                 <CardBody>
                     <Row className='g-1 filter-row'>
@@ -79,7 +83,7 @@ const View = () => {
                         <Col className='mb-1 col-auto'>
                             <Col className="row g-0">
                                 <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">Supplier name</Label>
+                                    <Label htmlFor="select_supplier" className="me-50">Global Umbrella name</Label>
                                 </Col>
                                 <Col className="col-auto">
                                     <Col className="box-select server_data">
@@ -89,120 +93,42 @@ const View = () => {
                             </Col>
                         </Col>
 
-                        {/* <Col className='mb-1 col-auto'>
-                            <Col className="row g-0">
-                                <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">Article Number</Label>
-                                </Col>
-                                <Col className="col-auto">
-                                    <Col className="box-select server_data">
-                                    <Col className="server_data_label">{Artical_no}</Col>
-                                    </Col>
-                                </Col>
-                            </Col>
-                        </Col> */}
-
-                        {/* <Col className='mb-1 col-auto'>
-                            <Col className="row g-0">
-                                <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">Article Description</Label>
-                                </Col>
-                                <Col className="col-auto">
-                                    <Col className="box-select server_data">
-                                    <Col className="server_data_label"> {Art_description }</Col>
-                                    </Col>
-                                </Col>
-                            </Col>
-                        </Col> */}
-
-                        {/* <Col className='mb-1 col-auto'>
-                            <Col className="row g-0">
-                                <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">Category</Label>
-                                </Col>
-                                <Col className="col-auto">
-                                    <Col className="box-select server_data">
-                                    <Col className="server_data_label">{Art_category}</Col>
-                                    </Col>
-                                </Col>
-                            </Col>
-                        </Col> */}
-                        {/* </Row>
-                        <Row> */}
-
                         <Col className='mb-1 col-auto'>
                             <Col className="row g-0">
                                 <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">Current price</Label>
+                                    <Label htmlFor="select_supplier" className="me-50">Global Umbrella No</Label>
                                 </Col>
                                 <Col className="col-auto">
                                     <Col className="box-select server_data">
-                                    <Col className="server_data_label">{Current_price}</Col>
-                                    </Col>
-                                </Col>
-                            </Col>
-                        </Col>
-
-                        <Col className='mb-1 col-auto'>
-                            <Col className="row g-0">
-                                <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">New Price</Label>
-                                </Col>
-                                <Col className="col-auto">
-                                    <Col className="box-select server_data">
-                                    <Col className="server_data_label">{New_price}</Col>
-                                    </Col>
-                                </Col>
-                            </Col>
-                        </Col>
-
-                        <Col className='mb-1 col-auto'>
-                            <Col className="row g-0">
-                                <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">Request Date</Label>
-                                </Col>
-                                <Col className="col-auto">
-                                    <Col className="box-select server_data">
-                                    <Col className="server_data_label">{Request_date}</Col>
-                                    </Col>
-                                </Col>
-                            </Col>
-                        </Col>
-
-                        <Col className='mb-1 col-auto'>
-                            <Col className="row g-0">
-                                <Col className="col-auto d-flex align-items-center">
-                                    <Label htmlFor="select_supplier" className="me-50">Reason for Price Increase</Label>
-                                </Col>
-                                <Col className="col-auto">
-                                    <Col className="box-select server_data">
-                                    <Col className="server_data_label">{Reason_Price_change}</Col>
+                                    <Col className="server_data_label">{Supplierno}</Col>
                                     </Col>
                                 </Col>
                             </Col>
                         </Col>
                     </Row>
                     <Row>
-                    {countries.map(country => (
+                    {countries.map(row => (
                         <Col className="col-auto mt-1">
                             <Col className="country-info-box">
                                 <Col className="row">
                                     <Col className="col-auto align-self-end">
                                         <Col className="country-info-left">
                                             <Col className="country-name">
-                                                <span>{ country }</span>
+                                                <span>{ row.country_code }</span>
                                             </Col>
                                             <Col className="country-flag">
-                                                <img src={`${process.env.PUBLIC_URL}/flag/${country.toLowerCase()}.svg`}className="country-flag-img" />
+                                                <img src={`${process.env.PUBLIC_URL}/flag/${row.country_code.toLowerCase()}.svg`}className="country-flag-img" />
                                             </Col>
                                         </Col>
                                     </Col>
                                     <Col className="col-auto">
                                         <Col className="incr-infla d-flex justify-content-end">
-                                            <span className="incr-infla-badge incr-infla-1 mb-2">2%</span>
+                                            <span className="incr-infla-badge incr-infla-3 mb-2">{ row.requested_price_increase_perc === null ? 0 : row.requested_price_increase_perc }%</span>
+                                            <span className="incr-infla-badge incr-infla-5 mb-2">{ row.agreed_price_increase_perc === null ? 0 : row.agreed_price_increase_perc }%</span>
                                         </Col>
-                                        <Col className="sku-info d-flex justify-content-end align-bottom align-items-baseline align-items-end align-items">
-                                            6 SKU
+                                        
+                                        <Col onClick={(e) => (row.article_count > 0 ? handleEdit(e, row.country_name) : '')} className="sku-info d-flex justify-content-end align-bottom align-items-baseline align-items-end align-items">
+                                            {row.article_count ? row.article_count : 0} SKU
                                         </Col>
                                     </Col>
                                 </Col>
@@ -212,6 +138,7 @@ const View = () => {
                     </Row>
                 </CardBody>
             </Card>
+            <AddNewModalSupplier open={modal} handleModal={handleModal} rowData={rowData} />
         </Fragment>
     )
 }
