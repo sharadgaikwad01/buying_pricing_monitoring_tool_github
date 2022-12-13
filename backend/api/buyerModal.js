9bf71a58fc4b7d85c941e671dd2a109556f63084
Tathod <rahul.tathod@metro-gsc.in>
2022-12-13 14:09:30 +0530

    changes

--- backend/api/buyerModal.js
+++ backend/api/buyerModal.js
@@ -45,45 +45,51 @@ module.exports = function(app, con) {
     });
 
     app.post('/buyers_add_input', async function(req, res){
-            console.log(req.body)
-			if (req.body.stratbuyer_name) {
-				var sql = 'UPDATE dsp_contact SET active_status = ? WHERE buyer_emailid = ?';
-				con.query(sql, ['inactive', req.body.buyer_emailid], function (err, result) {
-					if (err) {
-						console.log("Update failure. Please try again.");
-						res.json({ status: false });
-						return;
-					};
-					if ( result.affectedRows == 0){
-						console.log("Invalid Data. No record has been updated.");
-						res.json({status:false});
-						return;
-					}
-				});
-
-				req.body.stratbuyer_name.forEach(function(row, key) {
-					option = { value: row.value, label: row.label }
-					articalIDOptions.push(option);
-				})
-			}
-			
-			// INSERT INTO tbl_buyer_details (first_name, last_name, dept_name, buyer_emailid, active_status, country_name, stratbuyer_name) VALUES 
-			// 							(req.body.first_name, req.body.last_name, req.body.dept_name, req.body.buyer_emailid, 'active', req.body.country_name, req.body.stratbuyer_name) 
-			// ON CONFLICT (buyer_emailid, stratbuyer_name) UPDATE SET first_name = EXCLUDED.first_name, last_name = EXCLUDED.last_name, dept_name = EXCLUDED.dept_name, active_status = 'active', stratbuyer_name=EXCLUDED.stratbuyer_name;
-
-
-        // sql=`CALL public.usp_addNewBuyers('` + req.body.first_name +`','`+ req.body.dept_name +`','`+ req.body.buyer_emailid +`','`+ req.body.emp_id +`','` + req.body.active_status + `','`+ req.body.country_name+`','` + req.body.stratbuyer_name + `');`;
-		// console.log(sql);
-		// con.query(sql, function(err, result) {
-		// 	if (err) {
-		// 		console.log(err);
-		// 		res.json({ status: false });
-		// 		return;
-		// 	}	
-		// });
         console.log(req.body)
-        // fetch all records
-        // var query = "SELECT * FROM public.tbl_users where action_status='Open'" + condition;
+		if (req.body.stratbuyer_name) {
+			var sql = 'UPDATE tbl_buyer_details SET active_status = ? WHERE buyer_emailid = ?';
+			await con.query(sql, ['inactive', req.body.buyer_emailid], function (err, result) {
+				if (err) {
+					console.log("Update failure. Please try again.");
+					res.json({ status: false });
+					return;
+				};
+				if ( result.affectedRows == 0){
+					console.log("Invalid Data. No record has been updated.");
+					res.json({status:false});
+					return;
+				}
+			});
+
+			req.body.stratbuyer_name.forEach( async function(row, key) {
+				option = { value: row.value, label: row.label }
+				articalIDOptions.push(option);
+
+			var getUniqueSupplierIdQuery = "select * from tbl_buyer_details WHERE buyer_emailid = ? AND stratbuyer_name=  ?";
+			await con.query(getUniqueSupplierIdQuery,[req.body.buyer_emailid, row.label ], async function(err, result1) {
+				if ( result1.affectedRows == 0){
+					var sql1 = 'INSERT INTO tbl_buyer_details (first_name,last_name,dept_name,buyer_emailid,country_name,startbuyer_id,startbuyer_name,active_status) VALUES (?, ?, ?, ?, ?, ?, ?)'
+					await con.query(sql1, [req.body.first_name, req.body.last_name, req.body.dept_name, req.body.country_name,row.value,row.label,'active'], function (err, result) {
+						if (err) {
+							console.log("Update failure. Please try again.");
+							res.json({ status: false });
+							return;
+						};
+					})
+				}else{
+					var sql1 = 'UPDATE tbl_buyer_details SET active_status = ? WHERE buyer_emailid = ? AND startbuyer_name = ?';
+					await con.query(sql1, ['active', req.body.buyer_emailid , row.label], function (err, result) {
+						if (err) {
+							console.log("Update failure. Please try again.");
+							res.json({ status: false });
+							return;
+						};
+					})
+
+				}
+			});	
+			})
+		}
 		var condition  = '';
         var articalIDOptions = [];
 		var getUniqueSupplierIdQuery = "select distinct stratbuyer_id, stratbuyer_name from tbl_stratbuyer_details";
@@ -96,12 +102,10 @@ module.exports = function(app, con) {
 					option = { value: value.stratbuyer_name, label: value.stratbuyer_name }
 					articalIDOptions.push(option);
 				});
-				//articalIDOptions = articalIDOptions;
             }
 		});	
 
         var query = "SELECT first_name, last_name, buyer_emailid, dept_name, country_name,string_agg(stratbuyer_name,', ') stratbuyer_name FROM public.tbl_buyer_details where buyer_emailid IS NOT NULL" + condition + " group by first_name, last_name, buyer_emailid, dept_name, country_name";
-		// var query="Select distinct first_name, last_name, buyer_emailid, dept_name, country_name"
         await con.query(query, function(err, result) {
 			if (err) {
 				res.json({ status: false });
